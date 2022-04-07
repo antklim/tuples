315527b6f997d619525e973749bf2380d292edb7
antklim <antklim@gmail.com>
2022-04-07 15:01:03 +1000

    Removed hardcoded implementation

--- tuples.go
+++ tuples.go
@@ -3,35 +3,57 @@ package tuples
 import (
 	"bufio"
 	"io"
+	"strings"
 )
 
-var f int
-
 type Reader struct {
-	r io.Reader
+	s *bufio.Scanner
 }
 
 func NewReader(r io.Reader) *Reader {
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+
 	return &Reader{
-		r: bufio.NewReader(r),
+		s: s,
 	}
 }
 
 func (r *Reader) Read() (tuple []string, err error) {
-	switch f {
-	case 0:
-		tuple = []string{"John", "Doe", "2000-01-01"}
-	case 1:
-		tuple = []string{"Bob", "Smith", "2010-10-10"}
-	default:
-		err = io.EOF
-	}
-	f++
-
-	return
+	return r.readTuple()
 }
 
 func (r *Reader) ReadAll() (tuples [][]string, err error) {
-	tuples = [][]string{{"John", "Doe", "2000-01-01"}, {"Bob", "Smith", "2010-10-10"}}
-	return
+	for {
+		tuple, err := r.readTuple()
+		if err == io.EOF {
+			return tuples, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		tuples = append(tuples, tuple)
+	}
+}
+
+func (r *Reader) readTuple() ([]string, error) {
+	if r.s.Scan() {
+		tuple := readFields(r.s.Text())
+		return tuple, nil
+	}
+	if err := r.s.Err(); err != nil {
+		return nil, err
+	}
+	return nil, io.EOF
+}
+
+func readFields(s string) []string {
+	fs := strings.Split(s, ",")
+
+	var fields []string
+	for _, v := range fs {
+		kv := strings.Split(v, "=")
+		fields = append(fields, kv[1])
+	}
+	return fields
 }
