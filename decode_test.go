6f6f7a29fc68eda73a372d721b18321f5b13e38b
antklim <antklim@gmail.com>
2022-04-29 15:31:17 +1000

    Adding more unmarshal test cases

--- decode_test.go
+++ decode_test.go
@@ -26,31 +26,77 @@ func TestInvalidUnmarshal(t *testing.T) {
 	}
 }
 
-func TestUnmarshal(t *testing.T) {
-	in := []byte("fname=John,lname=Doe,dob=2000-01-01 fname=Bob,lname=Smith,dob=2010-10-10")
-	type out struct {
-		Name        string `tuples:"fname"`
-		Surname     string `tuples:"lname"`
-		DateOfBirth string `tuples:"dob"`
-	}
-	want := []out{{
-		Name:        "John",
-		Surname:     "Doe",
-		DateOfBirth: "2000-01-01",
-	}, {
-		Name:        "Bob",
-		Surname:     "Smith",
-		DateOfBirth: "2010-10-10",
-	}}
+type T struct {
+	Name string `tuples:"name"`
+	// Age     int    `tuples:"age"`
+	// IsAdult bool   `tuples:"adult"`
+	Age     string `tuples:"age"`
+	IsAdult string `tuples:"adult"`
+}
 
-	var got []out
-	err := tuples.Unmarshal(in, &got)
-	if err != nil {
-		t.Fatalf("unexpected Unmarshal() error: %v", err)
+type unmarshalTest struct {
+	in  string
+	out []T
+	err error
+}
+
+var unmarshalTests = []unmarshalTest{
+	{in: "", out: []T{}},
+	{
+		in:  "name=John,lname=Doe,age=17",
+		out: []T{{Name: "John", Age: "17"}},
+	},
+	{
+		in:  "name=John,age=23,adult=true",
+		out: []T{{Name: "John", Age: "23", IsAdult: "true"}},
+	},
+	{
+		in: "name=John,age=23,adult=true name=Bob,adult=true adult=true,age=30",
+		out: []T{
+			{Name: "John", Age: "23", IsAdult: "true"},
+			{Name: "Bob", IsAdult: "true"},
+			{Age: "30", IsAdult: "true"},
+		},
+	},
+}
+
+func TestUnmarshal(t *testing.T) {
+	for _, tC := range unmarshalTests {
+		in := []byte(tC.in)
+		var got []T
+		err := tuples.Unmarshal(in, &got)
+		if err != nil {
+			t.Fatalf("unexpected Unmarshal() error: %v", err)
+		}
+		if !reflect.DeepEqual(got, tC.out) {
+			t.Fatalf("Unmarshal() output:\ngot %v\nwant %v", got, tC.out)
+		}
 	}
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("Unmarshal() output:\ngot %v\nwant %v", got, want)
-	}
+
+	// in := []byte("fname=John,lname=Doe,dob=2000-01-01 fname=Bob,lname=Smith,dob=2010-10-10")
+	// type out struct {
+	// 	Name        string `tuples:"fname"`
+	// 	Surname     string `tuples:"lname"`
+	// 	DateOfBirth string `tuples:"dob"`
+	// }
+	// want := []out{{
+	// 	Name:        "John",
+	// 	Surname:     "Doe",
+	// 	DateOfBirth: "2000-01-01",
+	// }, {
+	// 	Name:        "Bob",
+	// 	Surname:     "Smith",
+	// 	DateOfBirth: "2010-10-10",
+	// }}
+
+	// var got []out
+	// err := tuples.Unmarshal(in, &got)
+	// if err != nil {
+	// 	t.Fatalf("unexpected Unmarshal() error: %v", err)
+	// }
+	// if !reflect.DeepEqual(got, want) {
+	// 	t.Fatalf("Unmarshal() output:\ngot %v\nwant %v", got, want)
+	// }
 }
 
 // func TestUnmarshaSimple(t *testing.T) {
