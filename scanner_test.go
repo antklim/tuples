a57fa20e21e4f427e71d2b1ce395ab7814ff549f
antklim <antklim@gmail.com>
2022-06-12 16:19:40 +1000

    Code style updates

--- scanner_test.go
+++ scanner_test.go
@@ -15,36 +15,44 @@ type scanTest struct {
 	err  error
 }
 
-var scanTests = []scanTest{{
-	desc: "Empty input",
-}, {
-	desc: "Single tuple",
-	in:   "fname=John,lname=Doe,dob=2000-01-01",
-	out:  [][][]string{{{"fname", "John"}, {"lname", "Doe"}, {"dob", "2000-01-01"}}},
-}, {
-	desc: "Multiple tuples",
-	in:   "fname=John,lname=Doe,dob=2000-01-01 fname=Bob,lname=Smith,dob=2010-10-10",
-	out: [][][]string{
-		{{"fname", "John"}, {"lname", "Doe"}, {"dob", "2000-01-01"}},
-		{{"fname", "Bob"}, {"lname", "Smith"}, {"dob", "2010-10-10"}},
+var scanTests = []scanTest{
+	{
+		desc: "Empty input",
 	},
-}, {
-	desc: "Skip empty field",
-	in:   "fname=John,,dob=2000-01-01",
-	out:  [][][]string{{{"fname", "John"}, {"dob", "2000-01-01"}}},
-}, {
-	desc: "Invalid field #1",
-	in:   "fname=John,lname=Doe,dob=2000-01-01 name,lname=Smith,dob=2010-10-10",
-	err:  errors.New("tuples: scan failed: tuple #2 invalid field #1"),
-}, {
-	desc: "Invalid field #2",
-	in:   "fname=John,lname=Doe,dob=2000-01-01 name=,lname=Smith,dob=2010-10-10",
-	err:  errors.New("tuples: scan failed: tuple #2 invalid field #1"),
-}, {
-	desc: "Invalid field #3",
-	in:   "fname=John,lname=Doe,dob=2000-01-01 =Bob,lname=Smith,dob=2010-10-10",
-	err:  errors.New("tuples: scan failed: tuple #2 invalid field #1"),
-}}
+	{
+		desc: "Single tuple",
+		in:   "fname=John,lname=Doe,dob=2000-01-01",
+		out:  [][][]string{{{"fname", "John"}, {"lname", "Doe"}, {"dob", "2000-01-01"}}},
+	},
+	{
+		desc: "Multiple tuples",
+		in:   "fname=John,lname=Doe,dob=2000-01-01 fname=Bob,lname=Smith,dob=2010-10-10",
+		out: [][][]string{
+			{{"fname", "John"}, {"lname", "Doe"}, {"dob", "2000-01-01"}},
+			{{"fname", "Bob"}, {"lname", "Smith"}, {"dob", "2010-10-10"}},
+		},
+	},
+	{
+		desc: "Skip empty field",
+		in:   "fname=John,,dob=2000-01-01",
+		out:  [][][]string{{{"fname", "John"}, {"dob", "2000-01-01"}}},
+	},
+	{
+		desc: "Invalid field #1",
+		in:   "fname=John,lname=Doe,dob=2000-01-01 name,lname=Smith,dob=2010-10-10",
+		err:  errors.New("tuples: scan failed: tuple #2 invalid field #1"),
+	},
+	{
+		desc: "Invalid field #2",
+		in:   "fname=John,lname=Doe,dob=2000-01-01 name=,lname=Smith,dob=2010-10-10",
+		err:  errors.New("tuples: scan failed: tuple #2 invalid field #1"),
+	},
+	{
+		desc: "Invalid field #3",
+		in:   "fname=John,lname=Doe,dob=2000-01-01 =Bob,lname=Smith,dob=2010-10-10",
+		err:  errors.New("tuples: scan failed: tuple #2 invalid field #1"),
+	},
+}
 
 func TestNext(t *testing.T) {
 	for tI, tC := range scanTests {
@@ -68,10 +76,12 @@ func TestNext(t *testing.T) {
 				if err == nil || (err.Error() != tC.err.Error()) {
 					t.Errorf("#%d: scan next() error mismatch:\ngot  %v\nwant %v", tI, err, tC.err)
 				}
+
 				var e *ScannerError
 				if !errors.As(err, &e) {
 					t.Errorf("#%d: scan next() error is not a ScanError", tI)
 				}
+
 				if errors.Unwrap(e) == nil {
 					t.Errorf("#%d: scan next() error should wrap original error", tI)
 				}
@@ -79,6 +89,7 @@ func TestNext(t *testing.T) {
 				if err != nil {
 					t.Errorf("#%d: unexpected scan next() error: %v", tI, err)
 				}
+
 				if len(out) != len(tC.out) {
 					t.Errorf("#%d: scan next() output length mismatch:\ngot  %d\nwant %d", tI, len(out), len(tC.out))
 				} else {
@@ -101,35 +112,42 @@ type nextTest struct {
 	done  bool
 }
 
-var nextTests = []nextTest{{
-	desc:  "Scans first",
-	in:    "fname=John fname=Bob fname=Smith",
-	out:   [][]string{{"fname", "John"}},
-	times: 1,
-}, {
-	desc:  "Scans in provided position",
-	in:    "fname=John fname=Bob fname=Smith",
-	out:   [][]string{{"fname", "Bob"}},
-	times: 2,
-}, {
-	desc:  "Scans last",
-	in:    "fname=John fname=Bob fname=Smith",
-	out:   [][]string{{"fname", "Smith"}},
-	times: 3,
-}, {
-	desc:  "Safely scans till the end and stops",
-	in:    "fname=John fname=Bob fname=Smith",
-	times: 5,
-	done:  true,
-}, {
-	desc:  "Ignores zero times",
-	in:    "fname=John fname=Bob fname=Smith",
-	times: 0,
-}, {
-	desc:  "Ignores negative times",
-	in:    "fname=John fname=Bob fname=Smith",
-	times: -10,
-}}
+var nextTests = []nextTest{
+	{
+		desc:  "Scans first",
+		in:    "fname=John fname=Bob fname=Smith",
+		out:   [][]string{{"fname", "John"}},
+		times: 1,
+	},
+	{
+		desc:  "Scans in provided position",
+		in:    "fname=John fname=Bob fname=Smith",
+		out:   [][]string{{"fname", "Bob"}},
+		times: 2,
+	},
+	{
+		desc:  "Scans last",
+		in:    "fname=John fname=Bob fname=Smith",
+		out:   [][]string{{"fname", "Smith"}},
+		times: 3,
+	},
+	{
+		desc:  "Safely scans till the end and stops",
+		in:    "fname=John fname=Bob fname=Smith",
+		times: 5,
+		done:  true,
+	},
+	{
+		desc:  "Ignores zero times",
+		in:    "fname=John fname=Bob fname=Smith",
+		times: 0,
+	},
+	{
+		desc:  "Ignores negative times",
+		in:    "fname=John fname=Bob fname=Smith",
+		times: -10,
+	},
+}
 
 func TestNextTimes(t *testing.T) {
 	for tI, tC := range nextTests {
@@ -145,6 +163,7 @@ func TestNextTimes(t *testing.T) {
 			if err != nil {
 				t.Errorf("#%d: unexpected scan nextTimes() error: %v", tI, err)
 			}
+
 			if len(out) != len(tC.out) {
 				t.Errorf("#%d: scan nextTimes() output length mismatch:\ngot  %d\nwant %d", tI, len(out), len(tC.out))
 			} else {
@@ -154,6 +173,7 @@ func TestNextTimes(t *testing.T) {
 					}
 				}
 			}
+
 			if hasNext != !tC.done {
 				t.Errorf("#%d: scan nextTimes() hasNext mismatch:\ngot  %t\nwant %t", tI, hasNext, !tC.done)
 			}
@@ -168,34 +188,42 @@ type scannerOptTest struct {
 	err   error
 }
 
-var scannerOptTests = []scannerOptTest{{
-	desc:  "scanner with default settings",
-	sopts: scannerOptions{fd: ',', kvd: '='},
-}, {
-	desc:  "scanner with custom fields delimiter",
-	opts:  []scannerOption{withFieldsDelimiter(';')},
-	sopts: scannerOptions{fd: ';', kvd: '='},
-}, {
-	desc:  "scanner with custom key-value delimiter",
-	opts:  []scannerOption{withKeyValueDelimiter(':')},
-	sopts: scannerOptions{fd: ',', kvd: ':'},
-}, {
-	desc:  "scanner with custom fields and key-value delimiters",
-	opts:  []scannerOption{withFieldsDelimiter(';'), withKeyValueDelimiter(':')},
-	sopts: scannerOptions{fd: ';', kvd: ':'},
-}, {
-	desc: "scanner with the same delimiter is not valid",
-	opts: []scannerOption{withFieldsDelimiter(';'), withKeyValueDelimiter(';')},
-	err:  errors.New("tuples: invalid delimiters: fields and key-value delimiters are equal"),
-}, {
-	desc: "scanner with invalid fields delimiter",
-	opts: []scannerOption{withFieldsDelimiter(utf8.RuneError)},
-	err:  errors.New("tuples: invalid delimiters: invalid fields delimiter"),
-}, {
-	desc: "scanner with invalid key-value delimiter",
-	opts: []scannerOption{withKeyValueDelimiter(utf8.RuneError)},
-	err:  errors.New("tuples: invalid delimiters: invalid key-value delimiter"),
-}}
+var scannerOptTests = []scannerOptTest{
+	{
+		desc:  "scanner with default settings",
+		sopts: scannerOptions{fd: ',', kvd: '='},
+	},
+	{
+		desc:  "scanner with custom fields delimiter",
+		opts:  []scannerOption{withFieldsDelimiter(';')},
+		sopts: scannerOptions{fd: ';', kvd: '='},
+	},
+	{
+		desc:  "scanner with custom key-value delimiter",
+		opts:  []scannerOption{withKeyValueDelimiter(':')},
+		sopts: scannerOptions{fd: ',', kvd: ':'},
+	},
+	{
+		desc:  "scanner with custom fields and key-value delimiters",
+		opts:  []scannerOption{withFieldsDelimiter(';'), withKeyValueDelimiter(':')},
+		sopts: scannerOptions{fd: ';', kvd: ':'},
+	},
+	{
+		desc: "scanner with the same delimiter is not valid",
+		opts: []scannerOption{withFieldsDelimiter(';'), withKeyValueDelimiter(';')},
+		err:  errors.New("tuples: invalid delimiters: fields and key-value delimiters are equal"),
+	},
+	{
+		desc: "scanner with invalid fields delimiter",
+		opts: []scannerOption{withFieldsDelimiter(utf8.RuneError)},
+		err:  errors.New("tuples: invalid delimiters: invalid fields delimiter"),
+	},
+	{
+		desc: "scanner with invalid key-value delimiter",
+		opts: []scannerOption{withKeyValueDelimiter(utf8.RuneError)},
+		err:  errors.New("tuples: invalid delimiters: invalid key-value delimiter"),
+	},
+}
 
 func TestScannerOptions(t *testing.T) {
 	for tI, tC := range scannerOptTests {
@@ -206,13 +234,16 @@ func TestScannerOptions(t *testing.T) {
 				if err == nil || (err.Error() != tC.err.Error()) {
 					t.Errorf("#%d: newScanner() error mismatch:\ngot  %v\nwant %v", tI, err, tC.err)
 				}
+
 				var e *InvalidScannerOptionError
 				if !errors.As(err, &e) {
 					t.Errorf("#%d: newScanner() error is not a InvalidScannerOptionError", tI)
 				}
+
 				if errors.Unwrap(e) == nil {
 					t.Errorf("#%d: newScanner() error should wrap original error", tI)
 				}
+
 				if s != nil {
 					t.Errorf("#%d: newScanner() output:\ngot  %v\nwant nil", tI, s)
 				}
@@ -220,6 +251,7 @@ func TestScannerOptions(t *testing.T) {
 				if err != nil {
 					t.Fatalf("#%d: unexpected newScanner() error: %v", tI, err)
 				}
+
 				if !reflect.DeepEqual(s.opts, tC.sopts) {
 					t.Errorf("#%d: newScanner() invalid scanner options:\ngot %v\nwant %v", tI, s.opts, tC.sopts)
 				}
